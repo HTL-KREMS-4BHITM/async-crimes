3d4ca5354eb73bf99ef76697f19c15e05872ab76
Maxi-Huber <m.huber1@htlkrems.at>
2025-02-18 11:54:59 +0100

    [TASK] Repositories

---
+++ Domain/Interfaces/IRepositoryAsync.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain;
+
+public interface IRepositoryAsync<TEntity> where TEntity : class
+{
+    Task<TEntity> Create(TEntity t);
+
+    Task<List<TEntity>> CreateRange(List<TEntity> list);
+
+    Task Update(TEntity t);
+
+    Task UpdateRange(List<TEntity> list);
+
+    Task<TEntity>? Read(int id);
+
+    Task<List<TEntity>> Read(Expression<Func<TEntity, bool>> filter);
+
+    Task<List<TEntity>> Read(int start, int count);
+
+    Task<List<TEntity>> ReadAll();
+
+    Task Delete(TEntity t);
+}
---
+++ Domain/Repositories/ARepositoryAsync.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Domain.Repositories;
+
+public class ARepositoryAsync<TEntity> : IRepositoryAsync<TEntity> where TEntity : class
+{
+    private readonly CrimesContext _context;
+    private readonly DbSet<TEntity> _dbSet;
+
+    public ARepositoryAsync(CrimesContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<TEntity>();
+    }
+
+    public async Task<TEntity> Create(TEntity entity)
+    {
+        _dbSet.Add(entity);
+        _context.SaveChanges();
+        return entity;
+    }
+
+    public async Task<List<TEntity>> CreateRange(List<TEntity> entities)
+    {
+        _dbSet.AddRange(entities);
+        _context.SaveChanges();
+        return entities;
+    }
+
+    public async Task Update(TEntity entity)
+    {
+        _dbSet.Update(entity);
+        _context.SaveChanges();
+    }
+
+    public async Task UpdateRange(List<TEntity> entities)
+    {
+        _context.ChangeTracker.Clear();
+        _dbSet.UpdateRange(entities);
+        _context.SaveChanges();
+    }
+
+    public async Task<TEntity>? Read(int id)
+    {
+        return _dbSet.Find(id);
+    }
+
+    public async Task<List<TEntity>> Read(Expression<Func<TEntity, bool>> filter)
+    {
+        return _dbSet.Where(filter).ToList();
+    }
+
+    public async Task<List<TEntity>> Read(int start, int count)
+    {
+        return _dbSet.Skip(start).Take(count).ToList();
+    }
+
+    public async Task<List<TEntity>> ReadAll()
+    {
+        return _dbSet.ToList();
+    }
+
+    public async Task Delete(TEntity entity)
+    {
+        _dbSet.Remove(entity);
+        _context.SaveChanges();
+    }
+}
