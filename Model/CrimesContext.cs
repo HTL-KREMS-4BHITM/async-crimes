1d80cd9402717d2bfcdb9db9954e8c9a174af70f
werwolfy <raphael.nigl@outlook.de>
2025-02-18 11:53:33 +0100

    [TASK] Created OnModelCreating

--- Model/CrimesContext.cs
+++ Model/CrimesContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Model.Entities;
 
 namespace Model;
 
@@ -10,6 +11,54 @@ public class CrimesContext:DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<MercenaryReputation>()
+            .HasOne(c => c.Syndicate)
+            .WithMany()
+            .HasForeignKey(c => c.SyndicateId);
         
+        modelBuilder.Entity<MercenaryReputation>()
+            .HasOne(m => m.Mercenary)
+            .WithMany()
+            .HasForeignKey(m => m.MercenaryId);
+        
+        modelBuilder.Entity<AircraftCrewJT>()
+            .HasOne(a => a.Aircraft)
+            .WithMany()
+            .HasForeignKey(a => a.AircraftId);
+        
+        modelBuilder.Entity<AircraftCrewJT>()
+            .HasOne(m => m.Mercenary)
+            .WithMany()
+            .HasForeignKey(m => m.MercenaryId);
+        
+        modelBuilder.Entity<Aircraft>()
+            .HasOne(s => s.Specification)
+            .WithMany(a => a.Aircrafts)
+            .HasForeignKey(s => s.SpecificationId);
+        
+        modelBuilder.Entity<Compartment>()
+            .HasOne(a => a.Aircraft)
+            .WithMany(a => a.Compartments)
+            .HasForeignKey(a => a.AircraftId);
+        
+        modelBuilder.Entity<Machinery>()
+            .HasOne<Compartment>(m => m.Compartment)
+            .WithMany(c => c.Machineries)
+            .HasForeignKey(m => m.CompartmentId);
+        
+        modelBuilder.Entity<Machinery>()
+            .HasOne<Weapon>()
+            .WithOne(w => w.Machinery)
+            .HasForeignKey<Weapon>(w => w.MachineryId);
+        
+        modelBuilder.Entity<Machinery>()
+            .HasOne<EnergySystem>()
+            .WithOne(w => w.Machinery)
+            .HasForeignKey<EnergySystem>(w => w.MachineryId);
+        
+        modelBuilder.Entity<Machinery>()
+            .HasOne<EnvironmentalSystem>()
+            .WithOne(e => e.Machinery)
+            .HasForeignKey<EnvironmentalSystem>(e => e.MachineryId);
     }
 }
